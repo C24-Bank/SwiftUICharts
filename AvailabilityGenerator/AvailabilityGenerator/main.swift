6555fe67371ebaafee2e06e56a2a7f4b6230a2da
Mario Schreiner <mario.schreiner@c24.de>
2022-03-09 14:11:04 +0100

    Add AvailabilityGenerator

---
+++ AvailabilityGenerator/AvailabilityGenerator/main.swift
@@ -0,0 +1,35 @@
+#!/usr/bin/swift
+
+import Foundation
+
+let rootPath = "../../Sources"
+
+let enumerator = FileManager.default.enumerator(atPath: rootPath)!
+
+for file in enumerator {
+    let completePath = rootPath + "/" + (file as! String)
+    let fileURL = URL(fileURLWithPath: completePath)
+//    print("Looking at \(fileURL.absoluteString)")
+    
+    if fileURL.pathExtension.lowercased() == "swift" {
+        let contentData = try! Data(contentsOf: fileURL)
+        let content = String(data: contentData, encoding: .utf8)!
+        var newContent = ""
+        var changes = 0
+        content.enumerateLines { line, stop in
+            if line.starts(with: "class") || line.starts(with: "struct") || line.starts(with: "extension") {
+//                print(line)
+                newContent += "@available(iOS 14.0, *)\n"
+                changes += 1
+                
+            }
+            
+            newContent += line
+        }
+        
+        if changes > 0 {
+            try! newContent.write(to: fileURL, atomically: true, encoding: .utf8)
+            print("Made \(changes) changes to \(fileURL.absoluteString)")
+        }
+    }
+}
