01aa0818e3869cda59857f3b76dab519d5d54660
Mario Schreiner <mario.schreiner@c24.de>
2022-05-17 10:00:26 +0200

    Attempt to support disabling animations in doughnut charts

--- Sources/SwiftUICharts/PieChart/Views/DoughnutChart.swift
+++ Sources/SwiftUICharts/PieChart/Views/DoughnutChart.swift
@@ -57,7 +57,7 @@ public struct DoughnutChart<ChartData>: View where ChartData: DoughnutChartData 
                                  rect: geo.frame(in: .local))
                         .scaleEffect(startAnimation ? 1 : 0)
                         .opacity(startAnimation ? 1 : 0)
-                        .animation(Animation.spring().delay(Double(data) * 0.06))
+                        .animation(chartData.shouldAnimate ? Animation.spring().delay(Double(data) * 0.06) : nil)
                         .if(chartData.touchPointData == [chartData.dataSets.dataPoints[data]]) {
                             $0
                                 .scaleEffect(1.1)
