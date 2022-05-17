6f1967bd9d8493b4eda23ae165f2bd1413fdc8fa
Mario Schreiner <mario.schreiner@c24.de>
2022-05-17 17:27:09 +0200

    Fix build issues

--- Sources/SwiftUICharts/PieChart/Views/DoughnutChart.swift
+++ Sources/SwiftUICharts/PieChart/Views/DoughnutChart.swift
@@ -52,24 +52,9 @@ public struct DoughnutChart<ChartData>: View where ChartData: DoughnutChartData 
         return GeometryReader { geo in
             ZStack {
                 ForEach(chartData.dataSets.dataPoints.indices, id: \.self) { data in
-                    var amount = data < lastNumberOfDataPoints ? chartData.dataSets.dataPoints[data].amount : 0
-                    var startAngle = data < lastNumberOfDataPoints ? chartData.dataSets.dataPoints[data].startAngle : 0
+                    let animationDuration = animationDuration(for: chartData.dataSets.dataPoints[data], index: data)
+                    let animationDelay = animationDelay(for: chartData.dataSets.dataPoints[data], index: data)
                     
-                    while amount < 0 {
-                        amount += 2 * Double.pi
-                        startAngle += 2 * Double.pi
-                    }
-                    
-                    while startAngle < 0 {
-                        startAngle += 2 * Double.pi
-                        amount += 2 * Double.pi
-                    }
-                    
-                    let size = abs(amount-startAngle)
-                    let percentage = size * (15.91549430919/100)
-                    //15.91549430919
-                    let animationDuration = 1.0 * (percentage/100)
-                    let animationDelay = 1.0 * (startAngle * (15.91549430919/100))
                     DoughnutSegmentShape(id: chartData.dataSets.dataPoints[data].id,
                                          startAngle: data < lastNumberOfDataPoints ? chartData.dataSets.dataPoints[data].startAngle : 0,
                                          amount: data < lastNumberOfDataPoints ? chartData.dataSets.dataPoints[data].amount : 0)
@@ -100,5 +85,51 @@ public struct DoughnutChart<ChartData>: View where ChartData: DoughnutChartData 
         .onDisappear {
             self.startAnimation = false
         }
+    }
+    
+    private func animationDuration(for dp: PieChartDataPoint, index: Int) -> Double {
+        var amount = index < lastNumberOfDataPoints ? dp.amount : 0
+        var startAngle = index < lastNumberOfDataPoints ? dp.startAngle : 0
+        
+        while amount < 0 {
+            amount += 2 * Double.pi
+            startAngle += 2 * Double.pi
+        }
+        
+        while startAngle < 0 {
+            startAngle += 2 * Double.pi
+            amount += 2 * Double.pi
+        }
+        
+        let size = abs(amount-startAngle)
+        let percentage = size * (15.91549430919/100)
+        //15.91549430919
+        let animationDuration = 1.0 * (percentage/100)
+        let animationDelay = 1.0 * (startAngle * (15.91549430919/100))
+        
+        return animationDuration
+    }
+    
+    private func animationDelay(for dp: PieChartDataPoint, index: Int) -> Double {
+        var amount = index < lastNumberOfDataPoints ? dp.amount : 0
+        var startAngle = index < lastNumberOfDataPoints ? dp.startAngle : 0
+        
+        while amount < 0 {
+            amount += 2 * Double.pi
+            startAngle += 2 * Double.pi
+        }
+        
+        while startAngle < 0 {
+            startAngle += 2 * Double.pi
+            amount += 2 * Double.pi
+        }
+        
+        let size = abs(amount-startAngle)
+        let percentage = size * (15.91549430919/100)
+        //15.91549430919
+        let animationDuration = 1.0 * (percentage/100)
+        let animationDelay = 1.0 * (startAngle * (15.91549430919/100))
+        
+        return animationDelay
     }
 }
